ba508f0b636d04b1d0654698a6923e8469d2fd62
framirez <framirez@SMS.(none)>
2013-07-09 19:59:51 -0500

    grader.js for url

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "http://pure-escarpment-5511.herokuapp.com/";
+var sys = require('util'),
+    rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -33,6 +36,11 @@ var assertFileExists = function(infile) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    return instr;
+};
+
+var assertFileExists2 = function(infile) {
+    var instr = infile.toString();
     return instr;
 };
 
@@ -65,6 +73,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_of_page>', 'url to index.html', clone(assertFileExists2), URLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
